5c9219a8b9beb5a063f4c523c8bb467786117cec
BBOK <qhrud8527@yonsei.ac.kr>
2024-08-18 15:58:47 +0900

    Pb1

---
+++ Pb1/main.c
@@ -0,0 +1,27 @@
+//
+// Created by bbok on 8/18/24.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void write_TT(char* file, char* filename){
+  };
+
+int main() {
+    //write Truth table for 4-bit full adder
+    s0 = ...
+    s1 = ...
+    s2 = ...
+    s3 = ...
+    cout = ...
+    write_TT(s0, "s0.txt");
+    write_TT(s0, "s1.txt");
+    write_TT(s0, "s2.txt");
+    write_TT(s0, "s3.txt");
+    write_TT(cout, "s0.txt");
+
+    return 0;
+}
+
+
+//
